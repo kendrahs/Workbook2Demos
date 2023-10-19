7c553aef1c6352ac18376c1aa639fdc39434f97d
kendrah <kentinez11@gmail.com>
2023-10-19 08:48:23 -0400

    Demos

---
+++ demo.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var firstName;
+firstName = "Kendrah";
+console.log(firstName);
+
+firstName = "Ken"
+lastName = "Mart"
+
+
---
+++ demo3.js
@@ -0,0 +1,13 @@
+//excercise 1
+
+var customerId;
+var customerFullName;
+var gender;
+var dateOfBirth;
+var licenseNumber;
+var autoPolicyNumber;
+
+//excercise 3
+var name = "Kendrah Martinez"
+var cityBorn = "Nillie"
+var stateBorn = "Vic"
