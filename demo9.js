8290a1b1f0b1a315ab737c89ded50e4161e857d3
kendrah <kentinez11@gmail.com>
2023-10-19 09:59:17 -0400

    Demo 9, if else

---
+++ demo9.js
@@ -0,0 +1,10 @@
+let age = 42;
+let descriptionOfAge;
+
+if (age < 18) {
+    descriptionOfAge = "Underage";
+}
+else {
+    descriptionOfAge = "Adult"
+}
+// another way, could've ommitted curly braces
